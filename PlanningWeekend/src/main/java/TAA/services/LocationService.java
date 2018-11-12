2d42ef91bca5edaa360ad8d87e163534fbd596c8
AnassLalkha <lalkhaa1@gmail.com>
2018-11-12 21:17:41 +0100

    LocationService à jour

--- PlanningWeekend/src/main/java/TAA/services/LocationService.java
+++ PlanningWeekend/src/main/java/TAA/services/LocationService.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,28 +21,28 @@ import TAA.entities.Department;
 import TAA.entities.Location;
 
 @RestController
-@Transactional
+//@Transactional
 @RequestMapping("/locationService")
 //@Api(value= "PersonServiceApi", produces= MediaType.APPLICATION_JSON_VALUE)
 @CrossOrigin
 public class LocationService {
 	@Autowired
 	private LocationDao dao;
-	@RequestMapping("/Alllocations")
+	@RequestMapping(value="/all",method=RequestMethod.GET, produces="application/json")
 	@ResponseBody
 	public List<Location> AllLocations() {
 		return dao.findAll();
 	}  
 	
-	@PostMapping("/location/add")
+	@PostMapping("/add")
 	public void saveLocation(@RequestBody Location l) {
 		 dao.save(l);
 	}
-	@GetMapping(value = "/id/{id}")
+	@GetMapping(value = "/{id}")
     public Location getLocation(@PathVariable("id") int id) {
-		 return dao.findById(id);
+		 return dao.findOne(Long.valueOf(id));
     }
-	@DeleteMapping("/location/delete")
+	@DeleteMapping("/delete")
 	    public void deleteLocation(@RequestBody Location l) {
 	        dao.delete(l);    }
 	
